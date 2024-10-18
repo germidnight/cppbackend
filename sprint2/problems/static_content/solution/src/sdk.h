98d12028aa121ce44a61c8caed4c6ba9f50eca12
germidnight <germidnight@yandex.ru>
2024-10-18 21:20:33 +0300

    Sprint 2 static_content v4

--- sprint2/problems/static_content/solution/src/sdk.h
+++ sprint2/problems/static_content/solution/src/sdk.h
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 #pragma once
 #ifdef WIN32
 #include <sdkddkver.h>
 #endif
-=======
-#pragma once
-#ifdef WIN32
-#include <sdkddkver.h>
-#endif
->>>>>>> fcc504a15e4e401244d2e730709991971b06985c
