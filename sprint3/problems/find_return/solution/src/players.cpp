975072549a5f0862468d2343c865f728220d5728
germidnight <germidnight@yandex.ru>
2024-11-28 10:40:51 +0300

    Sprint 3 find-return, scores

---
+++ sprint3/problems/find_return/solution/src/players.cpp
@@ -0,0 +1,224 @@
+#include "players.h"
+
+#include <iomanip>
+#include <sstream>
+
+namespace players {
+
+namespace detail {
+    // длина строки всегда 32 hex цифры (128 бит)
+    std::string TokenTag::Serialize() {
+        const size_t len_of_64bit_word_in_hex = 16;
+        std::ostringstream os;
+        for (int i = 0; i < 2; ++i) {
+            os << std::hex << std::setfill('0') << std::setw(len_of_64bit_word_in_hex) << tag[i];
+        }
+        return os.str();
+    }
+
+} // namespace detail
+
+    std::shared_ptr<Token> PlayerTokens::AddPlayer(std::shared_ptr<Player> player) {
+        Token token(detail::TokenTag{generator1_(), generator2_()}.Serialize());
+        while (token_to_player_.count(token) > 0) {
+            token = Token(detail::TokenTag{generator1_(), generator2_()}.Serialize());
+        }
+        token_to_player_[token] = std::move(player);
+        return tokens_.emplace_back(std::make_shared<Token>(token));
+    }
+
+    std::shared_ptr<Player> PlayerTokens::FindPlayerByToken(const Token& token) const noexcept {
+        if (token_to_player_.count(token) > 0) {
+            return token_to_player_.at(token);
+        }
+        return nullptr;
+    }
+
+    /* Создание и добавление пользователя:
+     * 1) создаём и добавляем игрока с собакой в перечень игроков
+     * 2) в выбранной игровой сессии добавляем собаку нового игрока
+     */
+    std::shared_ptr<Player> Players::Add(std::string player_name,
+                                         std::shared_ptr<model::GameSession> game_session,
+                                         bool randomize_spawn_point) {
+        model::Map* game_map = game_session->GetMap();
+
+        std::shared_ptr<Player> player;
+        if (randomize_spawn_point) {
+	        player = players_.emplace_back(std::make_shared<Player>(model::Dog(++next_dog_id_, player_name,
+                                                        game_session->GetMap()->GetRandomPositionOnRoads()),
+                                                game_session));
+        } else {
+            player = players_.emplace_back(std::make_shared<Player>(model::Dog(++next_dog_id_, player_name,
+                                                        game_session->GetMap()->GetTestPositionOnRoads()),
+                                                game_session));
+        }
+        game_session->AddDog(player->GetDog());
+        map_id_to_index_.emplace(player->GetDog()->GetDogId(), players_.size() - 1);
+        return player;
+    }
+
+    std::shared_ptr<Player> Players::FindPlayerByDogId(size_t dog_id) const {
+        if (map_id_to_index_.count(dog_id) > 0) {
+            return players_[map_id_to_index_.at(dog_id)];
+        }
+        return nullptr;
+    }
+
+    /* Добавляем нового пользователя в игру
+     * 1) находим карту
+     * 2) получаем игровую сессию
+     * 3) добавляем пользователя
+     * 4) получаем токен пользователя */
+    JoinGameResult Application::JoinPlayerToGame(model::Map::Id map_id, std::string_view player_name) {
+        const model::Map* map = game_.FindMap(map_id);
+        if (map == nullptr) {
+            return JoinGameResult(nullptr, 0, JoinGameErrorCode::MAP_NOT_FOUND);
+        }
+
+        auto game_session = game_.PlacePlayerOnMap(map->GetId());
+        if (game_session == nullptr) {
+            return JoinGameResult(nullptr, 0, JoinGameErrorCode::SESSION_NOT_FOUND);
+        }
+
+        auto player = players_.Add(std::string(player_name), game_session, IsRandomSpawnPoint());
+        auto token = player_tokens_.AddPlayer(player);
+        return JoinGameResult(token.get(), player->GetDog()->GetDogId(), JoinGameErrorCode::NONE);
+    }
+
+    std::vector<std::shared_ptr<Player>> Application::GetPlayersInSession(const std::shared_ptr<Player> player) const {
+        std::vector<std::shared_ptr<Player>> res_players;
+
+        auto session = player->GetGameSession();
+        for (const auto next_dog : session->GetDogs()) {
+            auto next_player = players_.FindPlayerByDogId(next_dog->GetDogId());
+            if (next_player != nullptr) {
+                res_players.push_back(next_player);
+            }
+        }
+        return std::move(res_players);
+    }
+
+    void Application::SetDogAction(std::shared_ptr<Player> player, ActionMove action_move) {
+        auto dog_speed = player->GetGameSession()->GetMap()->GetSpeed();
+        auto dog = player->GetDog();
+        switch (action_move) {
+        case ActionMove::LEFT : {
+            dog->SetVelocity({-dog_speed, 0.});
+            dog->SetDirection(model::Direction::WEST);
+            break;
+        }
+        case ActionMove::RIGHT : {
+            dog->SetVelocity({dog_speed, 0.});
+            dog->SetDirection(model::Direction::EAST);
+            break;
+        }
+        case ActionMove::UP : {
+            dog->SetVelocity({0., -dog_speed});
+            dog->SetDirection(model::Direction::NORTH);
+            break;
+        }
+        case ActionMove::DOWN : {
+            dog->SetVelocity({0., dog_speed});
+            dog->SetDirection(model::Direction::SOUTH);
+            break;
+        }
+        default: {
+            dog->SetVelocity({0., 0.});
+            break;
+        }
+        }
+    }
+
+    /* Пересчёт событий на карте:
+     * 1) двигаем собак по картам
+     * 2) размещаем потерянные объекты в сессиях
+     * 3) отдаём находки в офис
+     * 4) подбираем предметы */
+    void Application::MoveDogs(double time_period) {
+        using namespace std::chrono_literals;
+        loot_gen::LootGenerator::TimeInterval duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                                                std::chrono::duration<double, std::milli>{time_period * 1s});
+
+        std::unordered_map<size_t, collision_detector::Gatherer> all_gatherers;
+        // перебор всех игроков
+        for (auto player : players_.GetPlayers()) {
+            model::DogState state = player->GetGameSession()->GetMap()->MoveDog(player->GetDog(), time_period);
+
+            all_gatherers[player->GetDog()->GetDogId()] = collision_detector::Gatherer{
+                                        player->GetDog()->GetDogState().position,
+                                        state.position,
+                                        model::LostObject::GATHERER_HALF_WIDTH};
+
+            player->GetDog()->SetState(state);
+        }
+        // перебор всех сессий
+        for (auto& map : game_.GetSessions()) {
+            for (auto session : map) {
+                // размещаем потерянные объекты в сессии
+                session->AddLostObjectsOnSession(game_.GetLootGenerator(), duration);
+
+                std::vector<collision_detector::Gatherer> gatherers;
+                std::unordered_map<size_t, std::shared_ptr<model::Dog>> idx_to_dog;
+                for (const auto& dog : session->GetDogs()) {
+                    gatherers.emplace_back(all_gatherers.at(dog->GetDogId()));
+                    idx_to_dog[gatherers.size() - 1] = dog;
+                }
+                BringItemsToOffices(session, gatherers, idx_to_dog);
+                PickUpItems(session, gatherers, idx_to_dog);
+            }
+        }
+    }
+
+    /* подбираем предметы:
+     * 1) формируем вектор items (gatherers сформирован выше) и передаём их провайдеру
+     * 2) получаем вектор событий подбора вещей собаками
+     * 3) для каждого события:
+     *      - подбираем собаками ещё не подобранные вещи, не забывая пометить подобранные вещи
+     * 4) удаляем подобранные вещи из списка потерянных */
+    void Application::PickUpItems(std::shared_ptr<model::GameSession> session,
+                                  const std::vector<collision_detector::Gatherer>& gatherers,
+                                  const std::unordered_map<size_t, std::shared_ptr<model::Dog>>& idx_to_dog) {
+        std::vector<std::shared_ptr<model::LostObject>> items(session->GetLostObjects().begin(),
+                                                              session->GetLostObjects().end());
+        ItemGatherer ig(items.size(), items, gatherers.size(), gatherers);
+        std::vector<bool> item_picked(session->CountLostObjects(), false);
+
+        for (const auto &event : collision_detector::FindGatherEvents(ig)) {
+            if (!item_picked[event.item_id]) {
+                item_picked[event.item_id] = idx_to_dog.at(event.gatherer_id)->AddPickedObject(
+                                                        model::PickedObject(items[event.item_id]->GetId(),
+                                                                            items[event.item_id]->GetType()),
+                                                        session->GetMap()->GetBagCapacity());
+            }
+        }
+        session->RemoveObjectsFromLost(item_picked); // удаляем только подобранные вещи
+    }
+
+    /* отдаём находки в офис
+     * 1) формируем вектор offices (gatherers сформирован выше) и передаём их провайдеру
+     * 2) получаем вектор событий посещения собаками офисов
+     * 3) для каждого события:
+     *      - сбрасываем все подобранные вещи */
+    void Application::BringItemsToOffices(std::shared_ptr<model::GameSession> session,
+                         const std::vector<collision_detector::Gatherer> &gatherers,
+                         const std::unordered_map<size_t, std::shared_ptr<model::Dog>> &idx_to_dog) {
+        std::vector<std::shared_ptr<model::LostObject>> offices;
+        for (const auto& office : session->GetMap()->GetOffices()) {
+            offices.emplace_back(std::make_shared<model::LostObject>(model::LostObject(0,
+                                                        {static_cast<double>(office.GetPosition().x),
+                                                        static_cast<double>(office.GetPosition().y)},
+                                                   offices.size(),
+                                                   model::LostObject::OFFICE_HALF_WIDTH)));
+        }
+        ItemGatherer og(offices.size(), offices, gatherers.size(), gatherers);
+        for (const auto& event : collision_detector::FindGatherEvents(og)) {
+            auto dog = idx_to_dog.at(event.gatherer_id);
+            if (dog->IsBagEmpty()) {
+                continue;
+            }
+            [[maybe_unused]]auto founded_objects = dog->FlushPickedObjects();
+        }
+    }
+
+} // namespace players
---
+++ sprint3/problems/scores/solution/src/game_session.h
@@ -0,0 +1,223 @@
+/*
+ * Модель игры
+ * - игровой персонаж
+ * - потерянные предметы
+ * - игровые сессии
+ */
+#pragma once
+#include "loot_generator.h"
+#include "tagged.h"
+
+#include <list>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace model {
+
+    class Map; // описан в model.h
+
+    enum class Direction {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    };
+
+    struct Position {
+        double x = 0.;
+        double y = 0.;
+    };
+
+    struct Velocity {
+        double x = 0.;
+        double y = 0.;
+    };
+
+    struct DogState {
+        Position position;
+        Velocity velocity;
+        Direction direction = Direction::NORTH;
+    };
+
+    class PickedObject {
+    public:
+        explicit PickedObject(const size_t obj_id, const size_t obj_type) noexcept
+                    : id_(obj_id)
+                    , type_(obj_type) {}
+        const size_t GetId() const noexcept {
+            return id_;
+        }
+        const size_t GetType() const noexcept {
+            return type_;
+        }
+    private:
+        size_t id_ = 0;
+        size_t type_ = 0;
+    };
+
+    /* --------------------------------------- Собака --------------------------------------- */
+    class Dog {
+    public:
+        explicit Dog(size_t id, std::string name, const Position& pos)
+                : id_(id)
+                , name_(name) {
+            state_.position = pos;
+        }
+
+        size_t GetDogId() const noexcept {
+            return id_;
+        }
+
+        const std::string& GetDogName() const noexcept {
+            return name_;
+        }
+
+        const DogState& GetDogState() const noexcept {
+            return state_;
+        }
+
+        void SetPosition(const Position& pos) {
+            state_.position = pos;
+        }
+
+        void SetVelocity(const Velocity& vel) {
+            state_.velocity = vel;
+        }
+
+        void SetDirection(const Direction& dir) {
+            state_.direction = dir;
+        }
+
+        void SetState(const DogState& state) {
+            state_ = state;
+        }
+
+        bool AddPickedObject(const PickedObject object, size_t bag_capacity);
+
+        const std::vector<PickedObject>& GetPickedObjects() const noexcept {
+            return objects_;
+        }
+
+        const bool IsBagEmpty() const noexcept {
+            return objects_.empty();
+        }
+
+        std::vector<PickedObject> FlushPickedObjects() {
+            std::vector<PickedObject> result(objects_.begin(), objects_.end());
+            objects_.clear();
+            return std::move(result);
+        }
+
+        const size_t GetScores() const noexcept {
+            return scores_;
+        }
+
+        void AddScores(size_t scores) {
+            scores_ += scores;
+        }
+
+    private:
+        size_t id_;
+        std::string name_;
+        DogState state_;
+        std::vector<PickedObject> objects_;
+        size_t scores_ = 0;
+    };
+
+    /* --------------------------------------- Потерянные вещи --------------------------------------- */
+    /* type_ - индекс в векторе model::Map::LootTypes
+     * position_ - положение на одной из дорог на карте*/
+    class LostObject {
+    public:
+        // половина ширины объектов
+        static constexpr double ITEM_HALF_WIDTH = 0.;
+        static constexpr double GATHERER_HALF_WIDTH = 0.3;
+        static constexpr double OFFICE_HALF_WIDTH = 0.25;
+
+        explicit LostObject(size_t type, Position position, size_t id, double width = ITEM_HALF_WIDTH)
+                    : type_(type)
+                    , position_(std::move(position))
+                    , id_(id)
+                    , width_(width) {}
+
+        size_t GetType() const noexcept{
+            return type_;
+        }
+        const Position& GetPosition() const noexcept {
+            return position_;
+        }
+        const size_t GetId() const noexcept {
+            return id_;
+        }
+        const double GetWidth() const noexcept {
+            return width_;
+        }
+
+    private:
+        size_t type_ = 0;
+        Position position_;
+        size_t id_; // присваивается автоматически при создании
+        double width_;
+    };
+
+    /* --------------------------------------- Игровая сессия --------------------------------------- */
+    class GameSession {
+    public:
+        using Dogs = std::vector<std::shared_ptr<Dog>>;
+        using LostObjects = std::list<std::shared_ptr<LostObject>>;
+
+	explicit GameSession(model::Map* map) : map_{map} {}
+
+        void AddDog(std::shared_ptr<Dog> dog) {
+            auto inserted_dog = dogs_.emplace_back(std::move(dog));
+            map_id_to_index_[inserted_dog->GetDogId()] = dogs_.size() - 1;
+        }
+
+        const Dogs& GetDogs() const noexcept {
+            return dogs_;
+        }
+
+        const Dog* FindDog(const size_t dog_id) const noexcept {
+            if (auto it = map_id_to_index_.find(dog_id); it != map_id_to_index_.end()) {
+                return dogs_.at(it->second).get();
+            }
+            return nullptr;
+        }
+
+        const size_t CountDogsInSession() const noexcept {
+            return dogs_.size();
+        }
+
+        Map* GetMap() noexcept {
+            return map_;
+        }
+
+        const size_t CountLostObjects() const noexcept {
+            return lost_objects_.size();
+        }
+
+        const LostObjects& GetLostObjects() noexcept {
+            return lost_objects_;
+        }
+        /* Удаление всех элементов списка, индексы которых отмечены true */
+        void RemoveObjectsFromLost(std::vector<bool> idxs_to_remove);
+
+        /* 1) Получаем от генератора количество новых потерянных предметов случайным образом.
+         * 2) Генерируем для каждого из них:
+         *      - Тип предмета — целое число от 0 до K−1 включительно, где K — количество элементов в массиве lootTypes
+         *      - Объект генерируется в случайно выбранной точке на случайно выбранной дороге карты. */
+        void AddLostObjectsOnSession(loot_gen::LootGenerator& loot_generator,
+                                    loot_gen::LootGenerator::TimeInterval time_delta);
+
+    private:
+        Dogs dogs_;
+        model::Map* map_;
+        std::unordered_map<size_t, size_t> map_id_to_index_; // <dog_id, index_in_dogs_>
+        LostObjects lost_objects_;
+        size_t last_object_id_ = 0;
+    };
+
+} // namespace model
